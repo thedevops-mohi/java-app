1f6cde8747c6eaa4a6c357ffef140af9666a8c45
Mohammed Yahaya <daudaauwal@yahoo.co.uk>
2024-06-06 00:29:52 -0400

    modified alot

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,13 +1,26 @@
 package com.example.helloworld;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "index";
+    @GetMapping("/")
+    public String helloWorld() {
+        String hostAddress = getHostAddress();
+        return "<html><body style='color:blue;'><h1>Hello, World!</h1><p>Your host IP address is: " + hostAddress + "</p></body></html>";
+    }
+
+    private String getHostAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return "Unknown Host";
+        }
     }
 }
