0078a07bc394eef0ee4101883d79787a0976e46e
Mohammed Yahaya <daudaauwal@yahoo.co.uk>
2024-10-22 17:55:07 -0400

    added k8s-yamls/

--- scripts/updateK8sManifest.sh
+++ scripts/updateK8sManifest.sh
@@ -2,57 +2,30 @@
 
 set -x
 
-# Check for required arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <deployment-name> <docker-image-name> <docker-image-tag>"
-    exit 1
-fi
-
 # Set the repository URL
 REPO_URL="https://$GITHUB_TOKEN@github.com/thedevops-mohi/java-app.git"
 
-# Remove existing directory if it exists
-if [ -d "/home/jenkins/repo" ]; then
-    rm -rf /home/jenkins/repo
-fi
-
-# Clone the git repository into the /home/jenkins/repo directory
-git clone "$REPO_URL" /home/jenkins/repo
+mkdir -p /tmp/temp/
+# Clone the git repository into the /tmp directory
+git clone "$REPO_URL" /tmp/temp/
 
 # Navigate into the cloned repository directory
-cd /home/jenkins/repo || { echo "Failed to navigate to repository"; exit 1; }
-
-# Check if the deployment file exists
-DEPLOYMENT_FILE="k8s-yamls/$1-deployment.yaml"
-if [ ! -f "$DEPLOYMENT_FILE" ]; then
-    echo "Deployment file $DEPLOYMENT_FILE does not exist."
-    exit 1
-fi
-
-# Set Git user configuration
-git config --global user.email "jenkins@example.com"
-git config --global user.name "Jenkins"
+cd /tmp/temp/
 
 # Make changes to the Kubernetes manifest file(s)
-echo "Updating image in $DEPLOYMENT_FILE"
-sed -i "s|image: .*|image: auwalu5/java_app:$2|g" "$DEPLOYMENT_FILE"
+# For example, let's say you want to change the image tag in a deployment.yaml file
+sed -i "s|image:.*|image: auwalu5/java_app:$2|g" k8s-yamls/$1-deployment.yaml
 
-# Check for changes
-if ! git diff-index --quiet HEAD --; then
-    # Add the modified files
-    git add .
+git status
 
-    # Commit the changes
-    git commit -m "Update Kubernetes manifest"
+# Add the modified filess
+git add .
 
-    # Pull the latest changes before pushing
-    git pull --rebase origin main
+# Commit the changes
+git commit -m "Update Kubernetes manifest"
 
-    # Push the changes back to the repository
-    git push -u origin --all 
-else
-    echo "No changes to commit."
-fi
+# Push the changes back to the repository
+git push -u origin  --all 
 
 # Cleanup: remove the temporary directory
-rm -rf /home/jenkins/repo
+rm -rf /tmp/temp/
